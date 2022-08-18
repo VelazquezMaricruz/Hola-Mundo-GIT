7395879f2ba45d437729a9481d583ace79fb5ff5
Maria <maricruzvelazquezmartinez01@gmail.com>
2022-08-18 14:30:48 -0400

    Se creo la funcion calcularEdad()

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 let nombre = "Maria Velazquez";
-let edad = 23;
+let edad = CalcularEdad(1998);
 console.log ("Hola "  +  nombre);
 console.log (nombre + " tiene "+ edad + " años");
+
+function CalcularEdad(anioNacimiento){
+    const fechaActual = new Date();
+    const anioActual = fechaActual.getFullYear();
+    const edad = anioActual-anioNacimiento;
+    return edad;
+} 
