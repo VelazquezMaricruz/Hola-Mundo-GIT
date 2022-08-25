74499270beae0a7b661819f79447d6df0b3542ea
Maria <maricruzvelazquezmartinez01@gmail.com>
2022-08-25 14:09:48 -0400

    Se agrego el funcion calcularSalario

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ let nombre = "Mari Cruz  Velazquez";
 let edad = CalcularEdad(1998);
 console.log ("Hola "  +  nombre);
 console.log (nombre + " tiene "+ edad + " años");
+console.log ("Se le pagara: "+calcularSalario(8, 30000));
 
 function CalcularEdad(anioNacimiento){
     return  new Date().getFullYear()-anioNacimiento;
 } 
+
+function calcularSalario(horasTrabajadas, montoPorHora){
+    return horasTrabajadas * montoPorHora;
+}
